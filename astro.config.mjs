87be4cb78aab83abf38b8344bedaf8304688b83d
LGsus113 <weikgoggowb@gmail.com>
2025-02-05 02:23:39 -0600

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import { resolve } from "path";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        "@": resolve("./src"),
+        "@public": resolve("./public"),
+      },
+    },
+  },
+});
