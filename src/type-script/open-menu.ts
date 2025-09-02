6408b8e3f293a347d203a6f030ee758deac5312d
LGsus113 <weikgoggowb@gmail.com>
2025-09-02 17:02:42 -0500

    separacion de logica

---
+++ src/type-script/open-menu.ts
@@ -0,0 +1,34 @@
+import { $ } from "@/type-script/dom-selector";
+
+function updateMenuState(button: HTMLElement, menu: HTMLElement) {
+  const isLgTailwind = window.innerWidth >= 1024;
+
+  if (isLgTailwind) {
+    button.setAttribute("aria-expanded", "false");
+    menu.classList.add("hidden");
+  }
+}
+
+function menuHover() {
+  const button = $("#menu-button");
+  const menu = $("#navbar-hamburger");
+
+  if (!button || !menu) return;
+
+  button.addEventListener("click", () => {
+    const isLgTailwind = window.innerWidth >= 1024;
+    if (isLgTailwind) return;
+
+    const isExpanded = button.getAttribute("aria-expanded") === "true";
+    button.setAttribute("aria-expanded", isExpanded ? "false" : "true");
+    menu.classList.toggle("hidden");
+  });
+
+  window.addEventListener("resize", () => updateMenuState(button, menu));
+  updateMenuState(button, menu);
+}
+
+export function menuInit() {
+  document.addEventListener("DOMContentLoaded", menuHover);
+  document.addEventListener("astro:after-swap", menuHover);
+}
